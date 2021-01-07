d2ab14da399ad626d22f39af2210184f89f40a21
LucasJordao <lucaswill12@hotmail.com>
2021-01-07 14:02:18 -0300

    Criando a tela da rota Bancos e o footer das paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BancosComponent } from './bancos/bancos.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BancosComponent,
-    ClientesComponent
+    ClientesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
